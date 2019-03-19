8e7106a03c73c5bedd424ad7ef9dbd5a0501b59f
SaLapus <42068375+SaLapus@users.noreply.github.com>
2019-03-19 22:19:19 +0300

    JS update
    Co-Authored-By: Гока <gok1002@users.noreply.github.com>

--- server/file_path/edaDrink.js
+++ server/file_path/edaDrink.js
@@ -1,17 +1,17 @@
-function openDrink(evt, drinkName) {
+function openEdaDrink(evt, edaDrink) {
 var i, tabcontent, tablinks;
 
 
-tabcontent = document.getElementsByClassName("buttonDrinkContent");
+tabcontent = document.getElementsByClassName("buttonEdaDrinkContent");
 for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
 }
 
-tablinks = document.getElementsByClassName("buttonDrinkLink");
+tablinks = document.getElementsByClassName("buttonEdaDrinkLink");
 for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
 }
 
-document.getElementById(drinkName).style.display = "block";
+document.getElementById(edaDrink).style.display = "block";
 evt.currentTarget.className += " active";
 }
